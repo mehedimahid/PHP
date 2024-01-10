0617bf3da0c329d45855c7629c0744668bdb431a
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2024-01-10 11:10:21 +0600

    empty value

--- Array/AssociativeArray/index2.php
+++ Array/AssociativeArray/index2.php
@@ -34,3 +34,27 @@ print_r($fullname);
 
 unset($fullname['lname']);
 print_r($fullname);
+
+//        //৩.৯ - এম্পটি ভ্যালু নিয়ে আলোচনা
+
+$name =0 ;
+
+if (isset($name)) {
+  echo "Name is set";
+}else{
+  echo "Name is not set";
+}
+echo"\n";
+
+if (empty($name)) {
+    echo "Name is empty";
+} else {
+    echo "Name is not empty";
+}
+echo "\n";
+
+if(isset($name) &&(is_numeric($name) || $name != '')){
+  echo "Name is set and  it's not empty ";
+}else{
+  echo"Name is either not set or it's empty";
+}
